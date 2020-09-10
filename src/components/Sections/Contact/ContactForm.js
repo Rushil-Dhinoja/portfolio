95597dc1fb9929ffb4b6b35c2d200fda47fdb36a
Rushil dhinoja <mr.rushil17@gmail.com>
2020-09-10 16:35:38 +0530

    Completed the desktop version

--- src/components/Sections/Contact/ContactForm.js
+++ src/components/Sections/Contact/ContactForm.js
@@ -8,15 +8,13 @@ const ContactForm = () => {
       method="POST"
       data-netlify="true"
     >
-      <div>
-        <input
-          type="text"
-          placeholder="Name"
-          id="name"
-          className="py-8 pl-16 border-radius-12 outline-none bg-secondary placeholder-teal-500 font-body placeholder-opacity-6 font-teal-500 border-width-1 border-solid border-transparent  focus-border-teal-500 width-100 "
-          name="Name"
-        />
-      </div>
+      <input
+        type="text"
+        placeholder="Name"
+        id="name"
+        className="py-8 pl-16 border-radius-12 outline-none bg-secondary placeholder-teal-500 font-body placeholder-opacity-6 font-teal-500 border-width-1 border-solid border-transparent  focus-border-teal-500 width-100 "
+        name="Name"
+      />
       <div>
         <input
           type="email"
@@ -26,15 +24,13 @@ const ContactForm = () => {
           className="py-8 pl-16 border-radius-12 outline-none  bg-secondary placeholder-teal-500 font-body placeholder-opacity-6 font-teal-500 border-width-1 border-solid border-transparent  focus-border-teal-500 width-100"
         />
       </div>
-      <div>
-        <textarea
-          rows="8"
-          className="py-8 pl-16 border-radius-12 outline-none roboto-regular bg-secondary placeholder-teal-500 font-body placeholder-opacity-6 font-teal-500 border-width-1 border-solid border-transparent width-100 focus-border-teal-500"
-          placeholder="Message"
-          name="message"
-          id="message"
-        ></textarea>
-      </div>
+      <textarea
+        rows="8"
+        className="py-8 pl-16 border-radius-12 outline-none roboto-regular bg-secondary placeholder-teal-500 font-body placeholder-opacity-6 font-teal-500 border-width-1 border-solid border-transparent width-100 focus-border-teal-500"
+        placeholder="Message"
+        name="message"
+        id="message"
+      ></textarea>
       <button
         type="submit"
         className="align-self-center mt-8 px-20 py-5 border-radius-10 outline-none border-width-0 bg-teal-500 font-white roboto-bold text-uppercase cursor-pointer transform hover:-translate-y-5 transition-duration-200"

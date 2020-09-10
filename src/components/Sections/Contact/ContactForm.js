cf86e7151d3a9f6868eaaddc63933348c9eb12e5
Rushil dhinoja <mr.rushil17@gmail.com>
2020-09-10 16:25:53 +0530

    trying contact form

--- src/components/Sections/Contact/ContactForm.js
+++ src/components/Sections/Contact/ContactForm.js
@@ -8,27 +8,33 @@ const ContactForm = () => {
       method="POST"
       data-netlify="true"
     >
-      <input
-        type="text"
-        placeholder="Name"
-        id="name"
-        className="py-8 pl-16 border-radius-12 outline-none bg-secondary placeholder-teal-500 font-body placeholder-opacity-6 font-teal-500 border-width-1 border-solid border-transparent  focus-border-teal-500  "
-        name="Name"
-      />
-      <input
-        type="email"
-        placeholder="Email"
-        name="email"
-        id="email"
-        className="py-8 pl-16 border-radius-12 outline-none  bg-secondary placeholder-teal-500 font-body placeholder-opacity-6 font-teal-500 border-width-1 border-solid border-transparent  focus-border-teal-500"
-      />
-      <textarea
-        rows="8"
-        className="py-8 pl-16 border-radius-12 outline-none roboto-regular bg-secondary placeholder-teal-500 font-body placeholder-opacity-6 font-teal-500 border-width-1 border-solid border-transparent  focus-border-teal-500"
-        placeholder="Message"
-        name="message"
-        id="message"
-      ></textarea>
+      <div>
+        <input
+          type="text"
+          placeholder="Name"
+          id="name"
+          className="py-8 pl-16 border-radius-12 outline-none bg-secondary placeholder-teal-500 font-body placeholder-opacity-6 font-teal-500 border-width-1 border-solid border-transparent  focus-border-teal-500  "
+          name="Name"
+        />
+      </div>
+      <div>
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          id="email"
+          className="py-8 pl-16 border-radius-12 outline-none  bg-secondary placeholder-teal-500 font-body placeholder-opacity-6 font-teal-500 border-width-1 border-solid border-transparent  focus-border-teal-500"
+        />
+      </div>
+      <div>
+        <textarea
+          rows="8"
+          className="py-8 pl-16 border-radius-12 outline-none roboto-regular bg-secondary placeholder-teal-500 font-body placeholder-opacity-6 font-teal-500 border-width-1 border-solid border-transparent  focus-border-teal-500"
+          placeholder="Message"
+          name="message"
+          id="message"
+        ></textarea>
+      </div>
       <button
         type="submit"
         className="align-self-center mt-8 px-20 py-5 border-radius-10 outline-none border-width-0 bg-teal-500 font-white roboto-bold text-uppercase cursor-pointer transform hover:-translate-y-5 transition-duration-200"
